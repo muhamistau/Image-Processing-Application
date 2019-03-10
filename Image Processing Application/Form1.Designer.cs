548d9842cceb2d86e990be8efce0bb9b7730d643
muhamistau <islam.taufikurahman@gmail.com>
2019-03-10 16:55:24 +0700

    Brightness - Thresholding

---
+++ Image Processing Application/Form1.Designer.cs
@@ -0,0 +1,392 @@
+﻿namespace Image_Processing_Application
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxBefore = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBoxAfter = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.brightnessButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxBrightness = new System.Windows.Forms.TextBox();
+            this.invertButton = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.gBtButton = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.gLumaButton = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.gHumanEyeButton = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.gLevelingButton = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBoxThreshold = new System.Windows.Forms.TextBox();
+            this.ThresholdButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBefore)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAfter)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBoxBefore
+            // 
+            this.pictureBoxBefore.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxBefore.Location = new System.Drawing.Point(12, 51);
+            this.pictureBoxBefore.Name = "pictureBoxBefore";
+            this.pictureBoxBefore.Size = new System.Drawing.Size(200, 200);
+            this.pictureBoxBefore.TabIndex = 0;
+            this.pictureBoxBefore.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openImageToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openImageToolStripMenuItem
+            // 
+            this.openImageToolStripMenuItem.Name = "openImageToolStripMenuItem";
+            this.openImageToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.openImageToolStripMenuItem.Text = "Open Image...";
+            this.openImageToolStripMenuItem.Click += new System.EventHandler(this.openImageToolStripMenuItem_Click);
+            // 
+            // pictureBoxAfter
+            // 
+            this.pictureBoxAfter.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxAfter.Location = new System.Drawing.Point(218, 51);
+            this.pictureBoxAfter.Name = "pictureBoxAfter";
+            this.pictureBoxAfter.Size = new System.Drawing.Size(200, 200);
+            this.pictureBoxAfter.TabIndex = 2;
+            this.pictureBoxAfter.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Before:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(215, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "After:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.brightnessButton);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBoxBrightness);
+            this.groupBox1.Location = new System.Drawing.Point(12, 257);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 89);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Brightness Control";
+            // 
+            // brightnessButton
+            // 
+            this.brightnessButton.Location = new System.Drawing.Point(119, 43);
+            this.brightnessButton.Name = "brightnessButton";
+            this.brightnessButton.Size = new System.Drawing.Size(75, 23);
+            this.brightnessButton.TabIndex = 2;
+            this.brightnessButton.Text = "Process";
+            this.brightnessButton.UseVisualStyleBackColor = true;
+            this.brightnessButton.Click += new System.EventHandler(this.brightnessButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Brightness Value";
+            // 
+            // textBoxBrightness
+            // 
+            this.textBoxBrightness.Location = new System.Drawing.Point(9, 45);
+            this.textBoxBrightness.Name = "textBoxBrightness";
+            this.textBoxBrightness.Size = new System.Drawing.Size(100, 20);
+            this.textBoxBrightness.TabIndex = 0;
+            // 
+            // invertButton
+            // 
+            this.invertButton.Location = new System.Drawing.Point(119, 39);
+            this.invertButton.Name = "invertButton";
+            this.invertButton.Size = new System.Drawing.Size(75, 23);
+            this.invertButton.TabIndex = 4;
+            this.invertButton.Text = "Process";
+            this.invertButton.UseVisualStyleBackColor = true;
+            this.invertButton.Click += new System.EventHandler(this.invertButton_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 44);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Invert Color";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.invertButton);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(12, 349);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 89);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Color Invertion";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.gBtButton);
+            this.groupBox3.Controls.Add(this.label8);
+            this.groupBox3.Controls.Add(this.gLumaButton);
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Controls.Add(this.gHumanEyeButton);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Controls.Add(this.gLevelingButton);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Location = new System.Drawing.Point(218, 257);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 181);
+            this.groupBox3.TabIndex = 7;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Grayscale";
+            // 
+            // gBtButton
+            // 
+            this.gBtButton.Location = new System.Drawing.Point(119, 129);
+            this.gBtButton.Name = "gBtButton";
+            this.gBtButton.Size = new System.Drawing.Size(75, 23);
+            this.gBtButton.TabIndex = 9;
+            this.gBtButton.Text = "Process";
+            this.gBtButton.UseVisualStyleBackColor = true;
+            this.gBtButton.Click += new System.EventHandler(this.gBtButton_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(6, 134);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(42, 13);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "BT.601";
+            // 
+            // gLumaButton
+            // 
+            this.gLumaButton.Location = new System.Drawing.Point(119, 100);
+            this.gLumaButton.Name = "gLumaButton";
+            this.gLumaButton.Size = new System.Drawing.Size(75, 23);
+            this.gLumaButton.TabIndex = 7;
+            this.gLumaButton.Text = "Process";
+            this.gLumaButton.UseVisualStyleBackColor = true;
+            this.gLumaButton.Click += new System.EventHandler(this.gLumaButton_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 105);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(67, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Luma/ITU-R";
+            // 
+            // gHumanEyeButton
+            // 
+            this.gHumanEyeButton.Location = new System.Drawing.Point(119, 71);
+            this.gHumanEyeButton.Name = "gHumanEyeButton";
+            this.gHumanEyeButton.Size = new System.Drawing.Size(75, 23);
+            this.gHumanEyeButton.TabIndex = 5;
+            this.gHumanEyeButton.Text = "Process";
+            this.gHumanEyeButton.UseVisualStyleBackColor = true;
+            this.gHumanEyeButton.Click += new System.EventHandler(this.gHumanEyeButton_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 76);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(113, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Human Eye Correction";
+            // 
+            // gLevelingButton
+            // 
+            this.gLevelingButton.Location = new System.Drawing.Point(119, 42);
+            this.gLevelingButton.Name = "gLevelingButton";
+            this.gLevelingButton.Size = new System.Drawing.Size(75, 23);
+            this.gLevelingButton.TabIndex = 3;
+            this.gLevelingButton.Text = "Process";
+            this.gLevelingButton.UseVisualStyleBackColor = true;
+            this.gLevelingButton.Click += new System.EventHandler(this.gLevelingButton_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 47);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Leveling Method";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.ThresholdButton);
+            this.groupBox4.Controls.Add(this.textBoxThreshold);
+            this.groupBox4.Controls.Add(this.label9);
+            this.groupBox4.Location = new System.Drawing.Point(424, 257);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(200, 181);
+            this.groupBox4.TabIndex = 8;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Thresholding";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(6, 29);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(100, 13);
+            this.label9.TabIndex = 2;
+            this.label9.Text = "Threshold Value (T)";
+            // 
+            // textBoxThreshold
+            // 
+            this.textBoxThreshold.Location = new System.Drawing.Point(9, 45);
+            this.textBoxThreshold.Name = "textBoxThreshold";
+            this.textBoxThreshold.Size = new System.Drawing.Size(100, 20);
+            this.textBoxThreshold.TabIndex = 3;
+            // 
+            // ThresholdButton
+            // 
+            this.ThresholdButton.Location = new System.Drawing.Point(119, 43);
+            this.ThresholdButton.Name = "ThresholdButton";
+            this.ThresholdButton.Size = new System.Drawing.Size(75, 23);
+            this.ThresholdButton.TabIndex = 4;
+            this.ThresholdButton.Text = "Process";
+            this.ThresholdButton.UseVisualStyleBackColor = true;
+            this.ThresholdButton.Click += new System.EventHandler(this.ThresholdButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBoxAfter);
+            this.Controls.Add(this.pictureBoxBefore);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBefore)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAfter)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxBefore;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openImageToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBoxAfter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxBrightness;
+        private System.Windows.Forms.Button brightnessButton;
+        private System.Windows.Forms.Button invertButton;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button gLevelingButton;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button gBtButton;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button gLumaButton;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button gHumanEyeButton;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button ThresholdButton;
+        private System.Windows.Forms.TextBox textBoxThreshold;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Image Processing Application/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Image_Processing_Application
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Bitmap originalBmp, resultBmp;
+
+        private void openImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files(*.jpg; *.bmp)|*.jpg; *.bmp";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                pictureBoxBefore.Image = new Bitmap(openFileDialog.FileName);
+            }
+        }
+
+        private void invertButton_Click(object sender, EventArgs e)
+        {
+            int rValue, gValue, bValue;
+            originalBmp = (Bitmap)pictureBoxBefore.Image;
+            int row = originalBmp.Width;
+            int column = originalBmp.Height;
+            resultBmp = new Bitmap(row, column);
+            Cursor = Cursors.WaitCursor;
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    rValue = 255 - originalBmp.GetPixel(i, j).R;
+                    gValue = 255 - originalBmp.GetPixel(i, j).G;
+                    bValue = 255 - originalBmp.GetPixel(i, j).B;
+
+                    resultBmp.SetPixel(i, j, Color.FromArgb(rValue, gValue, bValue));
+                }
+            }
+            pictureBoxAfter.Image = resultBmp;
+            Cursor = Cursors.Default;
+        }
+
+        private void brightnessButton_Click(object sender, EventArgs e)
+        {
+            int k = Convert.ToInt16(textBoxBrightness.Text);
+            //int i, j;
+            int rValue, gValue, bValue;
+            originalBmp = (Bitmap)pictureBoxBefore.Image;
+            int row = originalBmp.Width;
+            int column = originalBmp.Height;
+            resultBmp = new Bitmap(row, column);
+            Cursor = Cursors.WaitCursor;
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    rValue = originalBmp.GetPixel(i, j).R + k;
+                    gValue = originalBmp.GetPixel(i, j).G + k;
+                    bValue = originalBmp.GetPixel(i, j).B + k;
+
+                    if (rValue > 255) rValue = 255;
+                    if (gValue > 255) gValue = 255;
+                    if (bValue > 255) bValue = 255;
+
+                    if (rValue < 0) rValue = 0;
+                    if (gValue < 0) gValue = 0;
+                    if (bValue < 0) bValue = 0;
+
+                    resultBmp.SetPixel(i, j, Color.FromArgb(rValue, gValue, bValue));
+                }
+            }
+            pictureBoxAfter.Image = resultBmp;
+            Cursor = Cursors.Default;
+        }
+
+        private void gHumanEyeButton_Click(object sender, EventArgs e)
+        {
+            int rValue, gValue, bValue, grValue;
+            double temp;
+            originalBmp = (Bitmap)pictureBoxBefore.Image;
+            int row = originalBmp.Width;
+            int column = originalBmp.Height;
+            resultBmp = new Bitmap(row, column);
+            Cursor = Cursors.WaitCursor;
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    rValue = originalBmp.GetPixel(i, j).R;
+                    gValue = originalBmp.GetPixel(i, j).G;
+                    bValue = originalBmp.GetPixel(i, j).B;
+                    temp = (0.3 * rValue) + (0.59 * gValue) + (0.11 * bValue);
+                    grValue = (int)temp;
+                    resultBmp.SetPixel(i, j, Color.FromArgb(grValue, grValue, grValue));
+                }
+            }
+            pictureBoxAfter.Image = resultBmp;
+            Cursor = Cursors.Default;
+        }
+
+        private void gLumaButton_Click(object sender, EventArgs e)
+        {
+            int rValue, gValue, bValue, grValue;
+            double temp;
+            originalBmp = (Bitmap)pictureBoxBefore.Image;
+            int row = originalBmp.Width;
+            int column = originalBmp.Height;
+            resultBmp = new Bitmap(row, column);
+            Cursor = Cursors.WaitCursor;
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    rValue = originalBmp.GetPixel(i, j).R;
+                    gValue = originalBmp.GetPixel(i, j).G;
+                    bValue = originalBmp.GetPixel(i, j).B;
+                    temp = (0.2126 * rValue) + (0.7152 * gValue) + (0.0722 * bValue);
+                    grValue = (int)temp;
+                    resultBmp.SetPixel(i, j, Color.FromArgb(grValue, grValue, grValue));
+                }
+            }
+            pictureBoxAfter.Image = resultBmp;
+            Cursor = Cursors.Default;
+        }
+
+        private void gBtButton_Click(object sender, EventArgs e)
+        {
+            int rValue, gValue, bValue, grValue;
+            double temp;
+            originalBmp = (Bitmap)pictureBoxBefore.Image;
+            int row = originalBmp.Width;
+            int column = originalBmp.Height;
+            resultBmp = new Bitmap(row, column);
+            Cursor = Cursors.WaitCursor;
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    rValue = originalBmp.GetPixel(i, j).R;
+                    gValue = originalBmp.GetPixel(i, j).G;
+                    bValue = originalBmp.GetPixel(i, j).B;
+                    temp = (0.299 * rValue) + (0.587 * gValue) + (0.114 * bValue);
+                    grValue = (int)temp;
+                    resultBmp.SetPixel(i, j, Color.FromArgb(grValue, grValue, grValue));
+                }
+            }
+            pictureBoxAfter.Image = resultBmp;
+            Cursor = Cursors.Default;
+        }
+
+        private void ThresholdButton_Click(object sender, EventArgs e)
+        {
+            int rValue, gValue, bValue, grValue;
+            int t = Convert.ToInt16(textBoxThreshold.Text);
+            originalBmp = (Bitmap)pictureBoxBefore.Image;
+            int row = originalBmp.Width;
+            int column = originalBmp.Height;
+            resultBmp = new Bitmap(row, column);
+            Cursor = Cursors.WaitCursor;
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    rValue = originalBmp.GetPixel(i, j).R;
+                    gValue = originalBmp.GetPixel(i, j).G;
+                    bValue = originalBmp.GetPixel(i, j).B;
+                    grValue = (rValue + gValue + bValue) / 3;
+
+                    if (grValue > t) grValue = 255;
+                    else grValue = 0;
+
+                    resultBmp.SetPixel(i, j, Color.FromArgb(grValue, grValue, grValue));
+                }
+            }
+            pictureBoxAfter.Image = resultBmp;
+            Cursor = Cursors.Default;
+        }
+
+        private void gLevelingButton_Click(object sender, EventArgs e)
+        {
+            int rValue, gValue, bValue, grValue;
+            originalBmp = (Bitmap)pictureBoxBefore.Image;
+            int row = originalBmp.Width;
+            int column = originalBmp.Height;
+            resultBmp = new Bitmap(row, column);
+            Cursor = Cursors.WaitCursor;
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    rValue = originalBmp.GetPixel(i, j).R;
+                    gValue = originalBmp.GetPixel(i, j).G;
+                    bValue = originalBmp.GetPixel(i, j).B;
+                    grValue = (rValue + gValue + bValue) / 3;
+
+                    resultBmp.SetPixel(i, j, Color.FromArgb(grValue, grValue, grValue));
+                }
+            }
+            pictureBoxAfter.Image = resultBmp;
+            Cursor = Cursors.Default;
+        }
+    }
+}
